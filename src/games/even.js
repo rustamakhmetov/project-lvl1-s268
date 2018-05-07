035acbfe05a4cb100e98ee9c284b26f1fb77454f
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2018-05-07 16:03:27 +0500

    Refactoring

---
+++ src/games/even.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+import info, { welcome, getUsername } from '../utils';
+
+const getRandomInt = (min, max) => Math.floor((Math.random() * (max - (min + 1))) + min);
+
+const evenInt = num => num % 2 === 0;
+
+const isCorrectAnswer = (answer, value) => {
+  if (answer !== 'no' && answer !== 'yes') {
+    return false;
+  }
+  const isEven = evenInt(value);
+  return (isEven && answer === 'yes') || (!isEven && answer === 'no');
+};
+
+export default () => {
+  welcome('Answer "yes" if number even otherwise answer "no".');
+  const name = getUsername();
+  info(`Hello, ${name}!`);
+  for (let i = 0; i < 3; i += 1) {
+    const value = getRandomInt(1, 100);
+    info(`Question: ${value}`);
+    const answer = readlineSync.question('Your answer: ').trim();
+    if (!isCorrectAnswer(answer, value)) {
+      info('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
+      info(`Let's try again, ${name}!`);
+      return false;
+    }
+    info('Correct!');
+  }
+  info(`Congratulations, ${name}!`);
+  return true;
+};
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
-export default message => console.log(message);
+import readlineSync from 'readline-sync';
+
+const info = message => console.log(message);
+
+export const welcome = (description) => {
+  info('Welcome to the Brain Games!');
+  if (description !== undefined) {
+    info(description);
+  }
+};
+
+export const getUsername = () => readlineSync.question('May I have your name?: ');
+
+export default info;
