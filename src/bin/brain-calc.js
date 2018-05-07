a2ed9cb8b2f08624b5fd08e8b2a631a488d0ef57
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2018-05-07 15:57:37 +0500

    add binary 'brain-calc'

---
+++ src/bin/brain-calc.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import calculator from '../games/calculator';
+
+calculator();
---
+++ src/games/calculator.js
@@ -0,0 +1,7 @@
+import info from '../utils';
+
+const start = () => {
+  info('What is the result of the expression?');
+};
+
+export default start;
